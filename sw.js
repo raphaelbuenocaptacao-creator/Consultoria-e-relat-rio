bee5900cfb6e8a510f0bae3725f0a1e6c8b8e3d9
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-28 14:06:41 -0300

    pwa: restrict Consultoria runtime cache to app shell

--- sw.js
+++ sw.js
@@ -1,52 +1,39 @@
-const CACHE_VERSION = 'consultoria-relatorio-v1';
+const CACHE_VERSION = 'consultoria-relatorio-v2';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg'];
-
+const APP_SHELL = new Set(['./', './index.html', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg', './icons/icon-512-maskable.svg']);
 const PRIVATE_PATH = /\/(api|auth|login|logout|admin|session|sessions|token|tokens|account|profile|me)(\/|\?|$)/i;
 
 function isSafeRequest(request) {
-  if (request.method !== 'GET') return false;
-  if (request.headers.has('Authorization')) return false;
+  if (request.method !== 'GET' || request.headers.has('Authorization')) return false;
   const url = new URL(request.url);
-  if (url.origin !== self.location.origin) return false;
-  if (PRIVATE_PATH.test(url.pathname)) return false;
-  return true;
+  return url.origin === self.location.origin && !PRIVATE_PATH.test(url.pathname);
 }
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(caches.open(STATIC_CACHE).then(cache => cache.addAll([...APP_SHELL])).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== STATIC_CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== STATIC_CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   const { request } = event;
   if (!isSafeRequest(request)) return;
-  const url = new URL(request.url);
 
   if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request)
-        .then(response => response)
-        .catch(() => caches.match('./index.html'))
-    );
+    event.respondWith(fetch(request, { cache: 'no-store' }).catch(() => caches.match('./index.html')));
     return;
   }
 
-  event.respondWith(
-    caches.match(request).then(cached => {
-      const network = fetch(request).then(response => {
-        if (response && response.ok && response.type === 'basic') {
-          const copy = response.clone();
-          caches.open(STATIC_CACHE).then(cache => cache.put(request, copy));
-        }
-        return response;
-      }).catch(() => cached);
-      return cached || network;
-    })
-  );
+  const url = new URL(request.url);
+  const scopePath = new URL(self.registration.scope).pathname;
+  const relative = './' + url.pathname.slice(scopePath.length);
+  if (!APP_SHELL.has(relative)) return;
+
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
 });
